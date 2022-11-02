e17d88c5e5b6e943d8676c87c46c1539c5a0e3a1
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-02 11:17:43 +0100

    Creating Entities

---
+++ src/main/java/com/example/CustomQueries02/entities/Flight.java
@@ -0,0 +1,57 @@
+package com.example.CustomQueries02.entities;
+
+import jdk.jshell.Snippet;
+
+import javax.persistence.*;
+
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String description;
+    private String fromAirport;
+    private String toAirport;
+    @Enumerated(EnumType.STRING)
+    private Snippet.Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFromAirport() {
+        return fromAirport;
+    }
+
+    public void setFromAirport(String fromAirport) {
+        this.fromAirport = fromAirport;
+    }
+
+    public String getToAirport() {
+        return toAirport;
+    }
+
+    public void setToAirport(String toAirport) {
+        this.toAirport = toAirport;
+    }
+
+    public Snippet.Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Snippet.Status status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/example/CustomQueries02/entities/Status.java
@@ -0,0 +1,7 @@
+package com.example.CustomQueries02.entities;
+
+public enum Status {
+    ONTIME,
+    DELAYED,
+    CANCELLED
+}
